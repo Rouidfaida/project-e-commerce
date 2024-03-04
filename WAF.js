7cda4150f8eda3866bebe93122cc71f5653d8039
Rouidfaida <rouid.faida@yahoo.fr>
2024-03-04 12:08:40 +0100

    e-comm

--- WAF.js
+++ WAF.js
@@ -1,16 +1,20 @@
 // Middleware WAF.js
 const express = require('express');
-const app = express();
+
+// Remarquez qu'il n'y a pas de besoin de créer une instance d'express avec express() ici
+// Nous exportons simplement une fonction middleware
 
 // Importez vos helpers et configurations comme nécessaire
 // const { analyzeRequest, getIpDetails, logRequest } = require('./helpers');
 
-app.use(async (req, res, next) => {
-  // Obtenez l'IP du client, le User-Agent, etc.
-  const clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-  const userAgent = req.headers['user-agent'];
+const wafMiddleware = (req, res, next) => {
+    // Obtenez l'IP du client, le User-Agent, etc.
+    const clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    const userAgent = req.headers['user-agent'];
 
-  console.log(req)
+    console.log(req);
 
-  next();
-});
+    next();
+};
+
+module.exports = wafMiddleware;
