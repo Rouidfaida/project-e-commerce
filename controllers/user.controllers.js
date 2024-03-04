e320de7a291b97538a01dd2186f550810b7805ea
Rouidfaida <rouid.faida@yahoo.fr>
2024-03-04 11:37:19 +0100

    e-comm

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -58,7 +58,6 @@ exports.login=async(req,res)=>{
             email:theUser.email,
             role:theUser.userRole
         }
-        console.log(payload)
         let token =  jwt.sign(payload,secret)
         res.send({token,theUser});
     } catch (error) {
