d1da7f28f55edfa163f43e1703f0b5e80ae4b7a9
Rouidfaida <rouid.faida@yahoo.fr>
2024-03-04 12:04:50 +0100

    e-comm

--- Middleware WAF.js
+++ Middleware WAF.js
@@ -5,10 +5,10 @@ const app = express();
 // Importez vos helpers et configurations comme nécessaire
 
 app.use(async (req, res, next) => {
-//   // Obtenez l'IP du client, le User-Agent, etc.
-//   const clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-//   const userAgent = req.headers['user-agent'];
-
- console.log(req)
- console.log(res)
+  // Obtenez l'IP du client, le User-Agent, etc.
+  const clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  const userAgent = req.headers['user-agent'];
+console.log(req)
+console.log(res)
+  next();
 });
